8820940286650a384ecf1d892627eef2946c5ca0
Daniel Sader <daniel@4intelligence.com.br>
2019-11-18 13:17:47 -0200

    Added basic interrupt handling

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,7 +1,13 @@
 #include <drivers/screen.h>
+#include <cpu/isr.h>
+#include <cpu/idt.h>
 
-void main()
+#include "util.h"
+
+void main() 
 {
-    clear_screen();
-    kprint("Hello World!");
+    isr_install();
+    /* Test the interrupts */
+    __asm__ __volatile__("int $2");
+    __asm__ __volatile__("int $3");
 }
