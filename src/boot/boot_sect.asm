f4ac0546c61dd4aa41373c5842930ca8b3c84620
Daniel Sader <daniel@4intelligence.com.br>
2019-11-18 15:16:34 -0200

    Added simple commands

--- src/boot/boot_sect.asm
+++ src/boot/boot_sect.asm
@@ -49,9 +49,9 @@ BEGIN_PM:
 
 ; Global variables
 BOOT_DRIVE      db 0
-MSG_REAL_MODE   db "Started in 16-bit Real Mode", 0
-MSG_PROT_MODE   db "Switched to 32-bit Protected Mode", 0
-MSG_LOAD_KERNEL db "Loading kernel into memory...", 0
+MSG_REAL_MODE   db "Started in 16-bit Real Mode",13,10,0
+MSG_PROT_MODE   db "Switched to 32-bit Protected Mode",0
+MSG_LOAD_KERNEL db "Loading kernel into memory...",13,10,0
 ; 
 ; Padding and magic BIOS number
 ;
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -9,16 +9,31 @@ void main() {
     isr_install();
     irq_install();
 
+    clear_screen();
     kprint("Welcome to the OS!\n"
-        "Type END to halt the CPU\n> ");
+        "Type HELP for help\n> ");
 }
 
 void user_input(char *input) {
-    if (strcmp(input, "END") == 0) {
+    if (strcmp(input, "CLEAR") == 0) {
+        clear_screen();
+    }
+    else if (strcmp(input, "HELLO") == 0) {
+        kprint("Hello World!");
+    } else if (strcmp(input, "HELP") == 0) {
+        kprint("\nSystem Help\n");
+        kprint("\n");
+        kprint("CLEAR  Clears the screen\n");
+        kprint("HELLO  Hello World!\n");
+        kprint("HELP   This message\n");
+        kprint("EXIT   Quit the system\n");
+    } 
+    else if (strcmp(input, "END") == 0 || strcmp(input, "EXIT") == 0) {
         kprint("Stopping the CPU. Bye!\n");
         asm volatile("hlt");
+    } else {
+        kprint("Command not found: ");
+        kprint(input);
     }
-    kprint("You said: ");
-    kprint(input);
     kprint("\n> ");
 }
